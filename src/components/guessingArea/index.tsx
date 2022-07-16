b71dcc7a3511af1bf04f05313ff3804ef9fc282a
Sam Kelsey <sam_kelsey@hotmail.co.uk>
2022-07-16 19:55:59 +0100

    🚧 Started the colour inputs

--- src/components/guessingArea/index.tsx
+++ src/components/guessingArea/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.scss";
 
 import { IGuessUpdate } from "../../types/board";
@@ -9,13 +9,27 @@ interface IProps {
 }
 
 const GuessingArea = ({ updateGuess, submitGuess }: IProps) => {
+  const [col1, setCol1] = useState("#FFFFFF");
   return (
-    <>
-      <div>GuessingArea</div>
+    <div className="guessing-area">
+      <div className="colour-inputs">
+        <div className="colour-input">
+          <input
+            type="color"
+            value={col1}
+            onChange={(e) => setCol1(e.target.value)}
+          />
+          <div style={{ backgroundColor: col1 }}></div>
+        </div>
+        <div className="colour-input">
+          <input type="color" value="#FFFFFF" />
+          <div></div>
+        </div>
+      </div>
       <button type="submit" onClick={() => submitGuess()}>
         Submit Guess
       </button>
-    </>
+    </div>
   );
 };
 
